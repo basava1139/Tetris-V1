c01fe6c18960b8113dcbe0b600c1b7d44baeacf4
basava1139 <106962015+basava1139@users.noreply.github.com>
2024-03-10 09:28:32 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tetrice-app-deployment" # Replace with your actual S3 bucket name
+    bucket = "tetris-bucket-basu" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-southeast-2"
   }
